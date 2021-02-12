2ddd71a34c45789c68d5b263ca77edffd5117f95
dzsimran <78913397+dzsimran@users.noreply.github.com>
2021-02-12 19:59:00 +0530

    updated key
    updated key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   apiUrl: 'http://localhost:8089/',
   geoAPI: 'https://maps.googleapis.com/maps/api/geocode/json',
-  geoAPIKey: 'AIzaSyBxI5zTMPawNt4Rpihxt61Ub98GnWIOB4E',
+  geoAPIKey: '',
   apiVersion: 'v1',
   baseUrl: '/'
 };
